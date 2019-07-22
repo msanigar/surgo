1da7d6ef57962b63bbfcff448627d5ccbceb97ea
myles <myles.sanigar@ivendi.com>
2019-07-22 15:06:05 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Surgo.gg yo
+          Surgo.gg -- awesome pipeline done
         </p>
         <a
           className="App-link"
