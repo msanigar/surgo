1ecc271ef93a68046d72b040770d6f76a7567a12
myles <myles.sanigar@ivendi.com>
2019-07-22 12:50:54 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Test push
+          Test push again
         </p>
         <a
           className="App-link"
