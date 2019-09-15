9b4b29c18216fccdddccbe83a5fe16893b3f0146
myles <myles.sanigar@gmail.com>
2019-09-15 13:16:19 +0100

    Blog styling

--- client/pages/blog.js
+++ client/pages/blog.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import fetch from "isomorphic-unfetch";
 
+import Back from '../components/SVG/Back';
+import Chevron from '../components/SVG/Chevron';
+
 import "../styles/Main.scss";
 
 class Blog extends Component {
@@ -11,13 +14,28 @@ class Blog extends Component {
   render() {
     return (
       <div className="blog-container">
-        { this.props.wpData.posts.map((post, i) => {
+        <div className="top-section book-font">
+          <h1>"At their highest level, all competitive disciplines become vehicles for the expression of one's human potential." - <span> @Thooorin</span></h1>
+          <div
+            className="back book-font"
+            onClick={() => window.history.back()}
+          >
+            <Back />
+          </div>
+        </div>
+        <div className="next book-font">
+          <p>Read our blog</p>
+          <Chevron />
+          </div>
+        {this.props.wpData.posts.map((post, i) => {
+          let newDate = (new Date(post.date)).toLocaleString();
+          let formattedDate = newDate.replace(', ', ' at ')
           return (
             <div className="blog-post" key={i}>
-            { console.log(post)}
-            <h1> {post.title.rendered} </h1>
-            <span dangerouslySetInnerHTML={{ __html: post.content.rendered }}></span>
-            <p> { post.author_meta.user_nicename } </p>
+              {console.log(post)}
+              <h2> {post.title.rendered} </h2>
+              <span className="content book-font" dangerouslySetInnerHTML={{ __html: post.content.rendered }}></span>
+              <span className="meta">Posted by <em>{post.author_meta.user_nicename}</em><br /> on <em>{ formattedDate }</em></span>
             </div>
           )
         })}
@@ -26,7 +44,7 @@ class Blog extends Component {
   }
 }
 
-Blog.getInitialProps = async function() {
+Blog.getInitialProps = async function () {
   const pageRes = await fetch(
     "https://api.surgo.gg/wp-json/wp/v2/posts"
   );
