e9ad4e11aaa9b5517dd6fdd1253755e07ba3704d
myles <myles.sanigar@gmail.com>
2019-11-24 12:55:08 +0000

    Nav surgo home link

--- client/components/NavButton/NavButton.jsx
+++ client/components/NavButton/NavButton.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import Link from "next/link";
 
 const NavButton = ({ toggle, theme }) => {
   const classes = theme == "dark" ? "nav dark" : "nav";
   return (
     <div className={classes}>
       <div className="nav-wrapper">
-        <div className="nav-logo">
-          <p>Surgo</p>
-        </div>
+        <Link href={"/"}>
+          <div className="nav-logo">
+            <p>Surgo</p>
+          </div>
+        </Link>
+
         <div className="nav-toggle">
           Menu
           <input type="checkbox" id="nav-checkbox" onChange={() => toggle()} />
