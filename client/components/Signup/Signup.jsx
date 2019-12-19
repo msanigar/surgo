b4fe85060439611eb24fcdd5da329d895206e160
myles <myles.sanigar@gmail.com>
2019-12-19 19:57:33 +0000

    Typewriter, fix home menu, fix surgo logo nav

--- client/components/Signup/Signup.jsx
+++ client/components/Signup/Signup.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "../Button";
 import MailchimpSubscribe from "react-mailchimp-subscribe";
 
-const CustomForm = ({ status, message, onValidated, light }) => {
+const CustomForm = ({ status, message, onValidated, light, type }) => {
   let email;
   const submit = () =>
     email &&
@@ -13,12 +13,8 @@ const CustomForm = ({ status, message, onValidated, light }) => {
 
   return (
     <React.Fragment>
-      <div className="signup-form">
-        <input
-          ref={node => (email = node)}
-          type="email"
-          placeholder="Enter your email address here"
-        />
+      <div className='signup-form'>
+        <input ref={node => (email = node)} type='email' placeholder={type} />
         <div>
           {status === "sending" && <span>sending...</span>}
           {status === "error" && (
@@ -29,11 +25,11 @@ const CustomForm = ({ status, message, onValidated, light }) => {
           )}
         </div>
       </div>
-      <div className="btn-container">
+      <div className='btn-container'>
         <Button
           clickFunc={submit}
           theme={light && "dark"}
-          text="Submit"
+          text='Submit'
         ></Button>
       </div>
     </React.Fragment>
@@ -43,7 +39,47 @@ const CustomForm = ({ status, message, onValidated, light }) => {
 class Signup extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      text: "",
+      isDeleting: false,
+      loopNum: 0,
+      typingSpeed: 150
+    };
   }
+
+  componentDidMount() {
+    this.handleType();
+  }
+
+  handleType = () => {
+    const { isDeleting, loopNum, text, typingSpeed } = this.state;
+    const fullText = "Enter your email address here";
+
+    this.setState({
+      text: isDeleting
+        ? fullText.substring(0, text.length - 1)
+        : fullText.substring(0, text.length + 1),
+      typingSpeed: isDeleting ? 30 : 150
+    });
+
+    if (!isDeleting && text === fullText) {
+      setTimeout(
+        () =>
+          this.setState({
+            isDeleting: true
+          }),
+        500
+      );
+    } else if (isDeleting && text === "") {
+      this.setState({
+        isDeleting: false,
+        loopNum: loopNum + 1
+      });
+    }
+
+    setTimeout(this.handleType, typingSpeed);
+  };
 
   render() {
     const classes = this.props.light ? "light signup" : "signup";
@@ -51,7 +87,7 @@ class Signup extends React.Component {
       "https://gmail.us20.list-manage.com/subscribe/post?u=c628c648d6564223ae86c85b9&amp;id=d626b4e39f";
     return (
       <span className={classes}>
-        <p className="title">Join the experience</p>
+        <p className='title'>Join the experience</p>
         <MailchimpSubscribe
           url={url}
           render={({ subscribe, status, message }) => (
@@ -60,6 +96,7 @@ class Signup extends React.Component {
               message={message}
               light={this.props.light}
               onValidated={formData => subscribe(formData)}
+              type={this.state.text}
             />
           )}
         />
